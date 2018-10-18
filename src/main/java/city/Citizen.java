07db80adcaab5c59c036fb9b79a5cefdccbf352b
Tayfun Oztemel <tayfun.oztemel@lunatech.nl>
2018-10-18 15:35:40 +0200

    Lower visibility of Citizen

--- src/main/java/city/Citizen.java
+++ src/main/java/city/Citizen.java
@@ -2,9 +2,9 @@ package city;
 
 public class Citizen {
 
-    public final String name;
-    public final String event;
-    public final String rawMessage;
+    final String name;
+    final String event;
+    final String rawMessage;
 
     private Citizen(String rawMessage, String name, String event) {
         this.rawMessage = rawMessage;
@@ -18,28 +18,28 @@ public class Citizen {
         rawMessage = c.rawMessage;
     }
 
-    public static Citizen toCitizen(String rawMessage, String name) {
+    static Citizen toCitizen(String rawMessage, String name) {
         return new Citizen(rawMessage, name, rawMessage.split("-")[0]);
     }
 
-    public static Citizen toCitizen(String rawMessage) {
+    static Citizen toCitizen(String rawMessage) {
         String[] split = rawMessage.split("-");
         return new Citizen(rawMessage, split[1], split[0]);
     }
 
-    public static class Requeue extends Citizen {
+    static class Requeue extends Citizen {
         private Citizen c;
 
-        public Requeue(Citizen c) {
+        Requeue(Citizen c) {
             super(c);
             this.c = c;
         }
     }
 
-    public static class Drop extends Citizen {
+    static class Drop extends Citizen {
         private Citizen c;
 
-        public Drop(Citizen c) {
+        Drop(Citizen c) {
             super(c);
             this.c = c;
         }
